01caa8f7bf77244b94ee996f00c746e5d16105dc
mazznat <nathan.mazzilli@divtec.ch>
2022-02-04 15:28:31 +0100

    Création de la première page

--- app/src/main/java/com/mazznat/quizspeed/MainActivity.java
+++ app/src/main/java/com/mazznat/quizspeed/MainActivity.java
@@ -1,8 +1,13 @@
 package com.mazznat.quizspeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mainMenu = findViewById(R.id.main_menu);
+        setSupportActionBar(mainMenu);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_parametres:
+                break;
+            case R.id.menu_question:
+
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
