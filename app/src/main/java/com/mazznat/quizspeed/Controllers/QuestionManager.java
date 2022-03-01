f8e62b9c880344f4d7fd568e690f4b179ad02833
mazznat <nathan.mazzilli@divtec.ch>
2022-03-01 10:31:53 +0100

    Jeu java avec question aleatoire

--- app/src/main/java/com/mazznat/quizspeed/Controllers/QuestionManager.java
+++ app/src/main/java/com/mazznat/quizspeed/Controllers/QuestionManager.java
@@ -1,21 +1,33 @@
 package com.mazznat.quizspeed.Controllers;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.mazznat.quizspeed.Models.Question;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 
 public class QuestionManager {
-    private List<Question> questionList;
+    private ArrayList<Question> questionList = new ArrayList<>();
 
+    public QuestionManager() {
+        initQuestionList(questionList);
+    }
 
-    public List<Question> getQuestionList(){
+    public ArrayList<Question> getQuestionList(){
+
         return questionList;
     }
 
-    public Question getRandomQuestion(List<Question> questionList){
-        int randomIndex = getQuestionIndex(questionList);
+    public Question getRandomQuestion(ArrayList<Question> questionList){
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(questionList.size());
         Question question = questionList.get(randomIndex);
 
         questionList.remove(randomIndex);
@@ -23,12 +35,11 @@ public class QuestionManager {
         return question;
 
     }
-    private int getQuestionIndex(List<Question> questionList){
-        Random rand = new Random();
-        return rand.nextInt(questionList.size());
+    public boolean isEmpty (ArrayList<Question>listeQuestion){
+        return listeQuestion.isEmpty();
     }
 
-    private void initQuestionList(List<Question> questionList){
+    private void initQuestionList(ArrayList<Question> questionList){
         questionList.add(new Question("La capitale de l'Australie est Sydney",0));
         questionList.add(new Question("Un octet vaut 7 bits",0));
         questionList.add(new Question("Le phénomène à l'origine de l'univers est le Big Bong",0));
--- app/src/main/java/com/mazznat/quizspeed/GameActivity.java
+++ app/src/main/java/com/mazznat/quizspeed/GameActivity.java
@@ -3,6 +3,7 @@ package com.mazznat.quizspeed;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +11,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.TintableImageSourceView;
 
+import com.mazznat.quizspeed.Controllers.QuestionManager;
+import com.mazznat.quizspeed.Models.Question;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
@@ -21,11 +26,18 @@ public class GameActivity extends AppCompatActivity {
     private Button BT_rejouer;
     private TextView TV_Question1;
     private TextView TV_Question2;
-    private ArrayList<String> ListeQuestion = new ArrayList<>();
     private Button BT_Joueur1;
     private Button BT_Joueur2;
     private TextView TV_Score1;
     private TextView TV_Score2;
+
+
+    Handler handler;
+    Runnable questionRunnable=null;
+    QuestionManager questionManager = new QuestionManager();
+    Question questionEnCours;
+
+    ArrayList<Question> ListeQuestion = questionManager.getQuestionList();
 
     private void getId(){
         TV_nomJoueur1 = findViewById(R.id.nomJoueur1);
@@ -39,13 +51,19 @@ public class GameActivity extends AppCompatActivity {
         TV_Score1 = findViewById(R.id.ScoreJoueur1);
         TV_Score2 = findViewById(R.id.ScoreJoueur2);
 
+        BT_menu.setVisibility(View.INVISIBLE);
+        BT_rejouer.setVisibility(View.INVISIBLE);
+
+        BT_Joueur1.setEnabled(false);
+        BT_Joueur2.setEnabled(false);
+
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         getId();
 
         Intent GameActivity = getIntent();
@@ -53,68 +71,106 @@ public class GameActivity extends AppCompatActivity {
         String Joueur2 = GameActivity.getStringExtra("Joueur2");
 
 
-        ListeQuestion.add("Pytagore est-il grecque ?");
-        ListeQuestion.add("Y-a-t'il de l'oxygène dans l'espace ?");
-
-        BT_Joueur1.setEnabled(false);
-        BT_Joueur2.setEnabled(false);
-
-           // TV_Question1.setText(String.valueOf(ListeQuestion.get(1)));
-            //TV_Question2.setText(String.valueOf(ListeQuestion.get(1)));
-
-        for(int I = 5;I<=0;I--) {
-            TV_Question1.setText(String.valueOf(I));
-            TV_Question2.setText(String.valueOf(I));
-        }
-
-                new android.os.Handler().postDelayed(
-                        new Runnable() {
-                            public void run() {
-
-                                BT_Joueur1.setEnabled(true);
-                                BT_Joueur2.setEnabled(true);
-                            }
-                        }, 5000);
-
-
-
-
-
-
-
-
         TV_nomJoueur1.setText(Joueur1);
         TV_nomJoueur2.setText(Joueur2);
 
-        BT_menu.setVisibility(View.INVISIBLE);
-        BT_rejouer.setVisibility(View.INVISIBLE);
+        handler = new Handler();
+        questionRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (questionManager.isEmpty(questionManager.getQuestionList())) {
+                    BT_menu.setVisibility(View.VISIBLE);
+                    BT_rejouer.setVisibility(View.VISIBLE);
+                    BT_Joueur1.setEnabled(false);
+                    BT_Joueur2.setEnabled(false);
 
-        BT_Joueur1.setOnClickListener(new View.OnClickListener()
-        {
+                    if(Integer.parseInt(String.valueOf(TV_Score1.getText())) > Integer.parseInt(String.valueOf(TV_Score2.getText()))){
+                        TV_Question1.setText("Gagné ");
+                        TV_Question2.setText("Perdu");
+                    }
+                    else if(Integer.parseInt(String.valueOf(TV_Score2.getText())) > Integer.parseInt(String.valueOf(TV_Score1.getText()))){
+                        TV_Question2.setText("Gagné");
+                        TV_Question1.setText("Perdu");
+                    }else {
 
+                        TV_Question1.setText("Egalité !");
+                        TV_Question2.setText("Egalité !");
+                    }
+
+
+                    handler.removeCallbacks(this);
+
+                } else {
+                    Question question = questionManager.getRandomQuestion(ListeQuestion);
+                    String texteQuestion = question.getQuestion();
+                    TV_Question1.setText(texteQuestion);
+                    TV_Question2.setText(texteQuestion);
+                    BT_Joueur1.setEnabled(true);
+                    BT_Joueur2.setEnabled(true);
+
+                    questionEnCours = question;
+
+                    handler.postDelayed(this, 2000);
+                }
+            }
+        };
+        handler.postDelayed(questionRunnable, 2000);
+
+
+        BT_Joueur1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int ScoreInt = Integer.parseInt(TV_Score1.getText().toString());
-                ScoreInt++;
+                int reponse = questionEnCours.isReponse();
+                if(reponse == 1) {
+                    ScoreInt++;
+                }else
+                    if(ScoreInt != 0){
+                        ScoreInt--;
+                    }
+
                 TV_Score1.setText(String.valueOf(ScoreInt));
+                BT_Joueur1.setEnabled(false);
+                BT_Joueur2.setEnabled(false);
 
 
             }
         });
 
-        BT_Joueur2.setOnClickListener(new View.OnClickListener()
-        {
+        BT_Joueur2.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 int ScoreInt = Integer.parseInt(TV_Score2.getText().toString());
-                ScoreInt++;
+                int reponse = questionEnCours.isReponse();
+                if(reponse == 1) {
+                    ScoreInt++;
+                }else
+                    ScoreInt--;
                 TV_Score2.setText(String.valueOf(ScoreInt));
+                BT_Joueur1.setEnabled(false);
+                BT_Joueur2.setEnabled(false);
 
             }
         });
+        BT_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MainActivity = new Intent(GameActivity.this, MainActivity.class);
+                startActivity(MainActivity);
+            }
+        });
+
+        BT_rejouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gameActivity = new Intent(GameActivity.this, GameActivity.class);
+                gameActivity.putExtra("Joueur1", TV_nomJoueur1.getText().toString());
+                gameActivity.putExtra("Joueur2", TV_nomJoueur2.getText().toString());
+                startActivity(gameActivity);
+            }
+        });
     }
-
-
-
 }
+
+
