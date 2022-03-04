36b8d3c37f0dcec23a244369f128cd55d2d6a19d
mazznat <nathan.mazzilli@divtec.ch>
2022-03-04 15:44:06 +0100

    Base de données + xml du menu

--- app/src/main/java/com/mazznat/quizspeed/QuestionActivity.java
+++ app/src/main/java/com/mazznat/quizspeed/QuestionActivity.java
@@ -9,15 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class QuestionActivity extends AppCompatActivity {
 
-    private EditText ED_question;
-    private Switch VraiFaux;
+  private EditText ED_question;
+//    private Switch VraiFaux;
     private Button BT_Valider;
 
     private void getID(){
-        ED_question = findViewById(R.id.question_question);
-        VraiFaux = findViewById(R.id.switchQuestion);
+       ED_question = findViewById(R.id.question_question);
+//        VraiFaux = findViewById(R.id.switchQuestion);
         BT_Valider = findViewById(R.id.question_btnValider);
-    }
+   }
 
 
 
